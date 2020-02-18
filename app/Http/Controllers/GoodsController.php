d175ee285bb7e3115b74008d5075327d592ba24a
cep <2150876090@qq.com>
2020-02-18 01:54:39 +0000

    缓存

--- app/Http/Controllers/GoodsController.php
+++ app/Http/Controllers/GoodsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 use App\Model\Goods;
 
 class GoodsController extends Controller
@@ -10,6 +11,21 @@ class GoodsController extends Controller
     public function goods(){
         $goods_id=$_GET['goods_id'];
         // dump($goods_id);die;
+        $key=$goods_id;
+        echo 'redis:'.$key;echo '<br>';
+        $redis=Redis::get($key);
+        if($redis){
+            echo '有';
+            $abc=json_decode($cache,true);
+            print_r($abc);
+        }else{
+            echo '没有';
+            $abc=Goods::where('goods_id',$goods_id)->first();
+            $abc=json_encode($abc->toArray());
+            Redis::set($key,$abc);
+            Redis::expire($key,10);
+        }
+        die;
         $ip=$_SERVER['REMOTE_ADDR'];
         $ua=$_SERVER['HTTP_USER_AGENT'];
         $time=time();
